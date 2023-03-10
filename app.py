fc72df207c7f17cf0063ee58f1dff66d22551a1e
Dumisani Mbonani <dumbona022@student.wethinkcode.co.za>
2023-03-10 12:56:02 +0200

    BIG update for BASE_FRAMEWORK from version 1.0.0 to 2.0.0

---
+++ app.py
@@ -0,0 +1,15 @@
+from base import base
+
+
+
+# db = base.Database('people.db')
+# db.create_table('workers','firstName text,lastName text')
+# db.insert_data('workers',('Mick','Smith'))
+# db.update_data('workers', "firstName = 'Bob', lastName = 'Goodman'", "rowid = 1")
+# db.delete_data('workers','rowid = 1')
+
+# x = db.view_data('workers','rowid, firstName, lastName')
+# x = db.view_data('workers', where_clause = "rowid = 1")
+# x = db.view_data('workers','firstName', where_clause= "lastName = 'Smith'")
+
+# db.drop_table('workers')
