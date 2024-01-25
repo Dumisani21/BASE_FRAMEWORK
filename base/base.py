0a26d4f2b90446975a85726cc3b67a9362522561
Dumisani Mbonani <dumimboanani@gmail.com>
2024-01-25 16:27:36 +0200

    new changes

--- base/base.py
+++ base/base.py
@@ -6,9 +6,9 @@ class Database:
 		try:
 			self.conn = sqlite3.connect(db_name)
 			self.c = self.conn.cursor()
-			print(f"Connected to database '{db_name}'")
+			return True, f"Connected to database '{db_name}'"
 		except sqlite3.Error as e:
-			print(f"Error connecting to database: {e}")
+			return False, f"Error connecting to database: {e}"
 
 	def __del__(self):
 		self.conn.close()
@@ -16,9 +16,9 @@ class Database:
 	def create_table(self, table_name, attributes):
 		try:
 			self.c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({attributes})")
-			print(f"Table '{table_name}' created successfully")
+			return True, f"Table '{table_name}' created successfully"
 		except sqlite3.Error as e:
-			print(f"Error creating table: {e}")
+			return False, f"Error creating table: {e}"
 
 	def insert_data(self, table_name, data):
 		try:
@@ -26,33 +26,33 @@ class Database:
 			placeholders = ', '.join(placeholder * len(data))
 			self.c.execute(f"INSERT INTO {table_name} VALUES ({placeholders})", data)
 			self.conn.commit()
-			print(f"{self.c.rowcount} rows inserted successfully")
+			return True, f"{self.c.rowcount} rows inserted successfully"
 		except sqlite3.Error as e:
-			print(f"Error inserting data: {e}")
+			return False, f"Error inserting data: {e}"
 
 	def update_data(self, table_name, set_clause, where_clause):
 		try:
 			self.c.execute(f"UPDATE {table_name} SET {set_clause} WHERE {where_clause}")
 			self.conn.commit()
-			print(f"{self.c.rowcount} rows updated successfully")
+			return True, f"{self.c.rowcount} rows updated successfully"
 		except sqlite3.Error as e:
-			print(f"Error updating data: {e}")
+			return False, f"Error updating data: {e}"
 
 	def delete_data(self, table_name, where_clause):
 		try:
 			self.c.execute(f"DELETE FROM {table_name} WHERE {where_clause}")
 			self.conn.commit()
-			print(f"{self.c.rowcount} rows deleted successfully")
+			return True, f"{self.c.rowcount} rows deleted successfully"
 		except sqlite3.Error as e:
-			print(f"Error deleting data: {e}")
+			return False, f"Error deleting data: {e}"
 
 	def drop_table(self, table_name):
 		try:
 			self.c.execute(f"DROP TABLE IF EXISTS {table_name}")
 			self.conn.commit()
-			print(f"Table '{table_name}' dropped successfully")
+			return True, f"Table '{table_name}' dropped successfully"
 		except sqlite3.Error as e:
-			print(f"Error dropping table: {e}")
+			return False, f"Error dropping table: {e}"
 
 	def view_data(self, table_name, columns='*', where_clause=None):
 		try:
@@ -62,14 +62,14 @@ class Database:
 				self.c.execute(f"SELECT {columns} FROM {table_name}")
 			data = self.c.fetchall()
 			print(f"{len(data)} rows returned")
-			return data
+			return True, data
 		except sqlite3.Error as e:
-			print(f"Error viewing data: {e}")
+			return False, f"Error viewing data: {e}"
 
 	def get_tables_names(self, table_name):
 		try:
 			self.c.execute(f"SELECT * FROM {table_name}")
 			data = list(map(lambda x: x[0], self.c.description))
-			return data
+			return True, data
 		except sqlite3.Error as e:
-			return (f"Error: {e}")
+			return False, f"Error: {e}"
